700c3b3d6236b6bf397964197eb8b0ff744d50b2
Breno Valente Manhães <41521785+brenoppr@users.noreply.github.com>
2025-06-19 16:06:42 -0300

    Refactor tests to use pytest-flask

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,15 +1,9 @@
-from routes.llm import llm_bp
-from utils.test_utils import create_app
-
-app = create_app()
-app.register_blueprint(llm_bp)
+from flask.testing import FlaskClient
 
 
-def test_reset_chat_route(app):
-    with app.test_client() as client:
-        resp = client.get('/reset_chat')
+def test_reset_chat_route(client: FlaskClient):
+    resp = client.get('/reset_chat')
     assert resp.status_code == 200
     assert resp.data == b'OK'
 
-if __name__ == "__main__":
-    test_reset_chat_route(app)
+
